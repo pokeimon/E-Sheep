fdbde4d1c64ef78a0057e25b6f7cac5fbbc15351
Aaron Scott <AaronPScott@gmail.com>
2015-09-12 22:41:56 -0700

    Added Jumping!
    - Added basic jumping and a few platforms to jump on inside the
    environment.
    - Fixed bounds on camera

--- Assets/Scripts/MovementTest.cs
+++ Assets/Scripts/MovementTest.cs
@@ -3,19 +3,51 @@ using System.Collections;
 
 public class MovementTest : MonoBehaviour {
 
-	public float speed = 5f;
+	public float speed = 10f;
+    public float jumpSpeed = 100f;
+
+    private bool isOnPlatform = true;
+    private bool jump = true;
 	private Rigidbody2D body2d;
 
-	// Use this for initialization
-
-	void Start () {
+    // Use this for initialization
+	void Start ()
+    {
 		body2d = GetComponent<Rigidbody2D> ();
 	}
-	
-	// Update is called once per frame
 
-	void Update () {
-		var val = Input.GetAxis ("Horizontal");
-		body2d.velocity = new Vector2(speed * val, body2d.velocity.y);
+    //Checks if player is on a platform before it can jump
+    void OnCollisionEnter2D (Collision2D other)
+    {
+        if (other.gameObject.tag == "Platform")
+            isOnPlatform = true;
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Platform")
+            isOnPlatform = false;
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnPlatform)
+            jump = true;
+        else
+            jump = false;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Jump();
+		var horizontal = Input.GetAxis ("Horizontal");  
+		body2d.velocity = new Vector2(speed * horizontal, body2d.velocity.y);
 	}
+
+    void FixedUpdate ()
+    {
+        if(jump)
+            body2d.velocity = new Vector2(body2d.velocity.x, jumpSpeed);
+    }
 }
