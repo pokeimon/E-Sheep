d51c1ebc9c3746bfdbc14d9edac02c5515ce9200
ImonDaneshmand <imondaneshmand@gmail.com>
2015-09-17 11:38:44 -0700

    Added Comments the the Transitions script

--- Assets/Scripts/Transitions.cs
+++ Assets/Scripts/Transitions.cs
@@ -5,34 +5,39 @@ public class Transitions : MonoBehaviour
 {
 
     public Texture2D fadeTexture;   //The texture that will overlay the screen.  
-    public float fadeSpeed = 0.8f;
+    public float fadeSpeed = 0.8f;	
 
     private int drawDepth = -1000;
-    private float alpha = 1.0f; 
-    private int fadeDirection = -1;
+    private float alpha = 1.0f; 	
+    private int fadeDirection = -1; //-1 fadeIn or 1 fadeOut
 
 
     void OnGUI()
     {
         alpha += fadeDirection * fadeSpeed * Time.deltaTime;
-        alpha = Mathf.Clamp01(alpha);
-
+        alpha = Mathf.Clamp01(alpha);												//keeps the fading within 0 to 1
         GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
         GUI.depth = drawDepth;
-        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeTexture);
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeTexture);	//fills the screen with the texture
     }
+
+	/*used to fadIn the scene (typcally used at the start of a scene)
+	fadeSpeed is returned to use to time the transition of scenes*/
     public float FadeIn()
     {
         fadeDirection = -1;
         return fadeSpeed;
     }
 
+	/*used to fadeOut the scene (typcally used at the end of a scene)
+	fadeSpeed is returned to use to time the transition of scenes*/
     public float FadeOut()
     {
         fadeDirection = 1;
         return fadeSpeed;
     }
 
+	//This function is called after a new level was loaded.
     void OnLevelWasLoaded()
     {
         FadeIn();
