2c7e6e2e761efc4255981c6c05a0c26adab21340
ImonDaneshmand <imondaneshmand@gmail.com>
2015-09-16 20:46:42 -0700

    Fade now works / Created gamemaster (GM)
    We will use the GM for misc scripts that need to be in the scene and
    attached to an object that will never be destroyed.

--- Assets/Scripts/Transitions.cs
+++ Assets/Scripts/Transitions.cs
@@ -9,7 +9,7 @@ public class Transitions : MonoBehaviour
 
     private int drawDepth = -1000;
     private float alpha = 1.0f; 
-    private int fadeDirection;
+    private int fadeDirection = -1;
 
 
     void OnGUI()
@@ -17,7 +17,7 @@ public class Transitions : MonoBehaviour
         alpha += fadeDirection * fadeSpeed * Time.deltaTime;
         alpha = Mathf.Clamp01(alpha);
 
-        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b);
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
         GUI.depth = drawDepth;
         GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeTexture);
     }
