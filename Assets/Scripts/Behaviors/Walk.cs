9c0ed7e05b7cae660e62aade46c1986ae33655df
Blachowiak <Stblack004@gmail.com>
2015-09-16 18:09:41 -0700

    Implemented Input manager, BIG CHANGES read description
    I also changed the player sprite filter to point mode to make it look
    sharper, as well as changed the player collider to a poly collider. A
    lot of problems/bugs came from many changes I believe most of them have
    to do with the scaling that the tutorial uses being different from our
    scene.
    
    List of issues:
    - When FaceDirection is active the player sprite grows larger I can’t
    figure out why.
    - There is a strange delay on horizontal movement that causes the
    character to continue walking for a while after the key is lifted.
    - When jumping the CollisionState script is supposed to restrict jumps
    but is not working because I have not implemented a PlayerManager script
    that the tutorial uses to handle animation/behavior states.
    I did not want to change the scaling until I had Aaron go over this and
    see if he can find what is wrong.

---
+++ Assets/Scripts/Behaviors/Walk.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walk : AbstractBehavior {
+
+	public float speed = 5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		var right = inputState.GetButtonValue (inputButtons [0]);
+		var left = inputState.GetButtonValue (inputButtons [1]);
+
+		if (right || left) {
+
+			if (left){
+				inputState.direction = Directions.Left;
+			} else if (right) {
+				inputState.direction = Directions.Right;
+			}
+
+			var tmpSpeed = speed;
+
+			var velX = tmpSpeed * (float)inputState.direction;
+
+			body2d.velocity = new Vector2 (velX, body2d.velocity.y);
+		}
+	}
+}
---
+++ Assets/Scripts/Collision/CollisionState.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionState : MonoBehaviour {
+
+	public LayerMask collisionLayer;
+	public bool standing;
+	public Vector2 bottomPosition = Vector2.zero;
+	public float collisionRadius = 0.25f;
+	public Color debugcCollisionColor = Color.red;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUdate(){
+
+		var pos = bottomPosition;
+		pos.x += transform.position.x;
+		pos.y += transform.position.y;
+
+		standing = Physics2D.OverlapCircle (pos, collisionRadius, collisionLayer);
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.color = debugcCollisionColor;
+
+		var pos = bottomPosition;
+		pos.x += transform.position.x;
+		pos.y += transform.position.y;
+
+		Gizmos.DrawWireSphere (pos, collisionRadius);
+	}
+}
