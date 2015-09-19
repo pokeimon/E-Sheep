5247ca86c8ca13298f605c9d4107e2bdd1541974
gerar_000 <gerard.aguilar.156@my.csun.edu>
2015-09-19 11:48:10 -0700

    CameraFollow and Pause Menu
    Main Camera now has two scripts:
    1. CameraFollow (the old one that follows the character)
    2. PauseMenu which disables/enables the Pause Menu Canvas object (press
    Esc)

---
+++ Assets/Scripts/Options/PauseMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+	
+	public GameObject PauseUI;
+	
+	private bool paused = false;
+	
+	void Start(){
+		
+		PauseUI.SetActive (false);
+		
+	}
+	
+	void Update(){
+		
+		if (Input.GetButtonDown ("Pause")) {
+			paused = !paused;
+		}
+		
+		if (paused) {
+			PauseUI.SetActive (true);
+			Time.timeScale = 0; //sets time to 0, so nothing happens.
+		}
+		
+		if (!paused) {
+			PauseUI.SetActive (false);
+			Time.timeScale = 1;//if this is set to 0.3, it can create a slow motion effect.
+			
+		}
+		
+	}
+	
+}
