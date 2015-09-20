a53a9ba30a466318ba1481512dfa72b5ed1de811
Aaron Scott <AaronPScott@gmail.com>
2015-09-20 01:46:47 -0700

    Fixed Jumping
    - fixed collision problem allowing for normal jumping
    - added code to prevent it from jumping repeatedly if button held down
    - fixed jumping animation trigger (need separate animation with gun)
    - temporarily changed shoot animation key to 'p' for easier use while
    moving

--- Assets/Scripts/Behaviors/Jump.cs
+++ Assets/Scripts/Behaviors/Jump.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Jump : AbstractBehavior {
 
-	public float jumpSpeed = 15f;
+	public float jumpSpeed = 20f;
 	public Animator anim;
 
 	// Use this for initialization
@@ -14,9 +14,11 @@ public class Jump : AbstractBehavior {
 	// Update is called once per frame
 	void Update () {
 		var canJump = inputState.GetButtonValue (inputButtons [0]);
+        var holdTime = inputState.GetButtonHoldTime(inputButtons[0]);
+
 		anim.SetInteger("playerState", 0);
 		if (collisionState.standing) {
-			if(canJump){
+			if(canJump && holdTime < .1f){
 				OnJump();
 				anim.SetInteger("playerState", 2);
 			}
