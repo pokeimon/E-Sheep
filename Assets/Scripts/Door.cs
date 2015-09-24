153d8718e0dab421b74df5504eb5e9ab66575ede
Ivan <ivan5247@gmail.com>
2015-09-24 10:37:40 -0700

    Door update
    Added fade out

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -13,9 +13,17 @@ public class Door : MonoBehaviour {
 	void OnTriggerStay2D(Collider2D p){
 		if(p.CompareTag("Player")){//Look for player tag
 			if(Input.GetKeyDown("w") || Input.GetKeyDown("up")){
-				Application.LoadLevel(LevelLoad);//if 'w' or 'up' keys are pressed it loads next level
+				StartCoroutine(FadeStartLevel());
+				//Application.LoadLevel(LevelLoad);//if 'w' or 'up' keys are pressed it loads next level
+
 			}
 		}
+	}
+
+	IEnumerator FadeStartLevel(){
+		float fadeTime = GameObject.Find("GM").GetComponent<Transitions>().FadeOut();
+		yield return new WaitForSeconds(fadeTime);
+		Application.LoadLevel(LevelLoad);
 	}
 
 
