0b0e0ba7e9553241bb36b830fd629fa105b412ca
Imon Daneshmand <imondaneshmand@gmail.com>
2015-09-28 11:26:52 -0700

    Added transition to the logo screen
    yup, need to update scene numbers throughout our game.

---
+++ Assets/Scripts/Startup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Startup : MonoBehaviour {
+	public float sceneTimeLegnth = 2.0f;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(wait());
+	}
+
+	IEnumerator wait(){
+		yield return new WaitForSeconds(sceneTimeLegnth);
+		StartCoroutine(GameObject.Find("GM").GetComponent<Transitions>().FadeStartLevel(0));
+	}
+
+}
