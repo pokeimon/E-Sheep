347bec7db4e7d75abfd05186b035e7005a788bcb
Imon Daneshmand <imondaneshmand@gmail.com>
2015-10-14 09:59:42 -0700

    Created a Basic Moving Platform
    Followed the tutorial: https://www.youtube.com/watch?v=U1UkKw12pQo

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+	
+	public Transform platform;
+	public Transform startTransform;
+	public Transform endTransform;
+	public float platformSpeed = 2;
+
+	Vector3 direction;
+	Transform destination;
+
+	void Start(){
+		SetDestination(startTransform);
+	}
+
+
+	void FixedUpdate(){
+		platform.GetComponent<Rigidbody>().MovePosition(platform.position + direction * platformSpeed * Time.fixedDeltaTime);
+
+		if(Vector3.Distance(platform.position, destination.position) < platformSpeed * Time.fixedDeltaTime){
+			SetDestination(destination == startTransform ? endTransform : startTransform);
+		}
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube(startTransform.position, platform.localScale);
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireCube(endTransform.position, platform.localScale);
+	}
+
+	void SetDestination(Transform dest){
+		destination = dest;
+		direction = (destination.position - platform.position).normalized;
+	}
+}
