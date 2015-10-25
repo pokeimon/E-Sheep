5e4144844eb09f057b3ffa22002782b7d78c1908
Megarpie <megankim314@gmail.com>
2015-10-25 12:47:15 -0700

    Testing player warping
    Does not work fully yet

---
+++ Assets/Scripts/Warp.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Warp : MonoBehaviour {
+
+	public Transform warpTarget;
+
+	void OnTriggerEnter2D (Collider2D other) {
+
+		//other.gameObject.transform.position = warpTarget.position; //changes position of player
+		//Camera.main.transform.position = warpTarget.position; //changes position of camera
+
+	}
+
+}
