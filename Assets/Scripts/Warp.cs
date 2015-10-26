64c6b6f393c828af7ce3c1ead0fa645d5075194a
Ivan <ivan5247@gmail.com>
2015-10-25 22:25:43 -0700

    Camera Positioning between rooms
    Changed some stuff in the Warp script to transition between rooms

--- Assets/Scripts/Warp.cs
+++ Assets/Scripts/Warp.cs
@@ -5,12 +5,18 @@ public class Warp : MonoBehaviour {
 
 	public Transform warpTarget;
 
+	public Camera theCamera;
+
+	public Vector3 playerPosition;
+
 	void OnTriggerEnter2D (Collider2D other) {
 
 		if (other.name == "Player") {
 			other.gameObject.transform.position = warpTarget.position; //changes position of player
 			//Camera.main.transform.position = warpTarget.position; //changes position of camera //might not need this line
-
+			playerPosition = other.gameObject.transform.position;
+			playerPosition.z = -2;
+			theCamera.transform.position = playerPosition;
 		}
 
 	}
