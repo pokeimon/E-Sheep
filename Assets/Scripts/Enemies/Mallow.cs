fc2a796b880a929f680d08d7908758caf0156046
Blachowiak <Stblack004@gmail.com>
2015-11-22 13:35:21 -0800

    Finally got Mallows chase to work
    - It is buggy, but I finished it right before having to leave for work
    so this is all I can do for now.

--- Assets/Scripts/Enemies/Mallow.cs
+++ Assets/Scripts/Enemies/Mallow.cs
@@ -2,42 +2,58 @@ using UnityEngine;
 using System.Collections;
 
 public class Mallow : AbstractEnemy {
+	
+	public Rigidbody2D mallow;
+	public Transform target;
+	public bool chase = false;
 
 	void Start (){
 		speed = 3f;
 		jumpSpeed = 8f;
+		mallow = GetComponent<Rigidbody2D> ();
 	}
 
 	void FixedUpdate (){  //Handles movement and jumps
 
-		var tmpSpeed = speed;
-		var velX = tmpSpeed * (float)direction;
+		if (chase) {
+			Debug.Log ("Entered");
+			mallow.transform.position = Vector3.MoveTowards (mallow.transform.position, target.transform.position, Time.deltaTime * speed);
+		}
 		
-		body2d.velocity = new Vector2(velX, body2d.velocity.y);
+
 		
 		if ((Time.time % 2) == 0 && collisionState.standing) {
 			OnJump ();
 		}
 		
-		if ((Time.time % 4) == 0 && direction == enemyDirections.Right) {
-			//Debug.Log("Mallow direction changed: Left");
-			direction = enemyDirections.Left;
-		}
+		if (!chase) {
+			var tmpSpeed = speed;
+			var velX = tmpSpeed * (float)direction;
+			body2d.velocity = new Vector2 (velX, body2d.velocity.y);
+
+
+			if ((Time.time % 4) == 0 && direction == enemyDirections.Right) {
+				//Debug.Log("Mallow direction changed: Left");"
+				direction = enemyDirections.Left;
+			}
 		
-		if((Time.time % 8) == 0 && direction == enemyDirections.Left){
-			//Debug.Log("Mallow direction changed: Right");
-			direction = enemyDirections.Right;
+			if ((Time.time % 8) == 0 && direction == enemyDirections.Left) {
+				//Debug.Log("Mallow direction changed: Right");
+				direction = enemyDirections.Right;
+			}
 		}
 	}
-
 	protected virtual void OnTriggerEnter2D(Collider2D target) {
 		if (target.gameObject.tag == "Player") {
-			speed = 15f;
+			chase = true;
+			speed = 6f;
 		}
 	}
 	protected virtual void OnTriggerExit2D(Collider2D target) {
 		if (target.gameObject.tag == "Player") {
+			chase = false;
 			speed = 3f;
+			Debug.Log("Exited");
 		}
 	}
 
