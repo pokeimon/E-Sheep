0b673f2d44a092ea19733d9f15ca3b8fe9048cf3
Imon Daneshmand <imondaneshmand@gmail.com>
2015-11-23 12:09:29 -0800

    Fixed Warp in the Hub
    But he FLIES now

--- Assets/Scripts/Warp.cs
+++ Assets/Scripts/Warp.cs
@@ -11,6 +11,8 @@ public class Warp : MonoBehaviour {
 
 
 		if (other.name == "Player") {
+			Collider2D coll = GameObject.Find("Player").GetComponent<Collider2D>();
+			coll.attachedRigidbody.isKinematic = true;
 			float fadeTime = GameObject.Find("GM").GetComponent<Transitions>().FadeOut();	//fades out when player exits
 			yield return new WaitForSeconds (fadeTime);										//waits till fading out is finished
 			other.gameObject.transform.position = warpTarget.position;						//changes position of player
@@ -18,7 +20,8 @@ public class Warp : MonoBehaviour {
 			playerPosition.z = -2;												
 			theCamera.transform.position = playerPosition;									//moves camera
 			GameObject.Find("GM").GetComponent<Transitions>().FadeIn();						//fades in
-
+			GameObject.Find ("Player").GetComponent<Jump>().warp = false;
+			coll.attachedRigidbody.isKinematic = false;
 		}
 	}
 }
