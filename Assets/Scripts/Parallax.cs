8b80721bd6fe1853e959d08ffab01fae67ba1520
Imon Daneshmand <imondaneshmand@gmail.com>
2015-11-24 11:26:01 -0800

    Parallax works,

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -4,16 +4,19 @@ using System.Collections;
 public class Parallax : MonoBehaviour {
 
 	public Transform[] backgrounds;
-	public float smoothing;
+	public float smoothing = 1.0f;
 
 	private float[] parallaxScales;
 	private Transform cam;
 	private Vector3 previousCamPos;
 
+	void Awake(){
+		cam = Camera.main.transform;
+	}
+
 	// Use this for initialization
 	void Start () {
-		cam = Camera.main.transform;
-
+	
 		previousCamPos = cam.position;
 
 		parallaxScales = new float[backgrounds.Length];
@@ -22,13 +25,12 @@ public class Parallax : MonoBehaviour {
 			parallaxScales[i] = backgrounds[i].position.z * -1; 
 		}
 	}
-	
-	// Update is called once per frame
-	void LateUpdate () {
+
+	void Update () {
 		for(int i = 0; i < backgrounds.Length; i++){
 			float parallax = (previousCamPos.x - cam.position.x) * parallaxScales[i];
 
-			float backgroundTargetPosX = backgrounds[i].position.x - parallax;
+			float backgroundTargetPosX = backgrounds[i].position.x + parallax;
 
 			Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
 
