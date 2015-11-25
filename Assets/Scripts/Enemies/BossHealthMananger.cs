d2dbf9ba1dc9800674331bfdae35b3a625ddeeb0
Jorge Manzano <petu32@gmail.com>
2015-11-25 11:03:25 -0800

    updated BHM
    still needs work

--- Assets/Scripts/Enemies/BossHealthMananger.cs
+++ Assets/Scripts/Enemies/BossHealthMananger.cs
@@ -2,34 +2,42 @@
 using System.Collections;
 
 public class BossHealthMananger : MonoBehaviour {
-	public int enemyHealth;
 	public GameObject deathEffect;
+	private Health health;
+	private int HP;
+	private int maxHP;
+	private Rigidbody2D rb;
 	public GameObject bossPrefab;
 	public float minsize; //smallest size
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		health = GetComponent<Health> ();
+		rb = GetComponent<Rigidbody2D> ();
 	}
 	// Update is called once per frame
 	void Update () {
-	if (enemyHealth <= 0) {
+		HP = health.currentHP;
+		maxHP = health.maxHP;
+	if ( HP<=5) {
+			rb = GetComponent<Rigidbody2D> ();
 			Instantiate(deathEffect, transform.position, transform.rotation);
 			if(transform.localScale.y > minsize){
-			GameObject clone1 = Instantiate(bossPrefab, new Vector3 (transform.position.x +.05f, 
+			GameObject clone1 = Instantiate(bossPrefab, new Vector3 (transform.position.x + 1.5f, 
 				               transform.position.y, transform.position.z),transform.rotation)as GameObject;
-			GameObject clone2 = Instantiate(bossPrefab, new Vector3 (transform.position.x -.05f, 
+				rb = clone1.GetComponent<Rigidbody2D> ();
+				rb.isKinematic= false;
+			GameObject clone2 = Instantiate(bossPrefab, new Vector3 (transform.position.x - 1.5f, 
 				                transform.position.y, transform.position.z),transform.rotation)as GameObject;
+				rb = clone2.GetComponent<Rigidbody2D> ();
+				rb.isKinematic= false;
 				clone1.transform.localScale = new Vector3(transform.localScale.x *.5f, transform.localScale.y *.5f, transform.localScale.z);
-				clone1.GetComponent<BossHealthMananger>().enemyHealth =10;
+				clone1.GetComponent<BossHealthMananger>().HP =20;
 				clone2.transform.localScale = new Vector3(transform.localScale.x *.5f, transform.localScale.y *.5f, transform.localScale.z);
-				clone2.GetComponent<BossHealthMananger>().enemyHealth =10;
+				clone2.GetComponent<BossHealthMananger>().HP=20;
 			}
 				Destroy(gameObject);
 			}
 		}
-	public void giveDamage(int damageToGive){
-		enemyHealth -= damageToGive;
-	}
 
 
 	}
