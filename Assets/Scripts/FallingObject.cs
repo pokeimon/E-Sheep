1ac1d786bc76d4d550e5c10bb35db4dc541ae513
Imon Daneshmand <imondaneshmand@gmail.com>
2016-02-09 10:10:08 -0800

    Rough Falling Script
    the start of a falling script

---
+++ Assets/Scripts/FallingObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void  OnTriggerEnter2D(Collider other){
+		if (other.tag == "Player") {
+			GameObject.FindWithTag ("Ceilling").GetComponent<Rigidbody2D> ().isKinematic = false;
+		}
+	}
+}
