4eb1bfae44baac106369a433ff2034dea399dc7a
gerar_000 <gerard.aguilar.156@my.csun.edu>
2016-02-12 15:47:24 -0800

    Pathfinding Flying Candy
    Created:
    -PathFinding script (scans, then moves to available spots)
    -FlyingCandy script (temporary, doesn't use AbstractEnemy yet)
    -Radial script (needed to implement PathFinding)
    -Radial prefab

---
+++ Assets/Scripts/Enemies/FlyingCandy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//this is a temporary class. will have to readjust it to use AbstractEnemy
+public class FlyingCandy : MonoBehaviour {
+
+	Rigidbody2D myrigidbody;
+	int layerToCollideWith;
+
+	void Awake(){
+		myrigidbody = gameObject.GetComponentInParent<Rigidbody2D> ();
+		layerToCollideWith = 8;
+	}
+
+	void OnTriggerEnter2D(Collider2D other){//this one should prevent the monster from going outside the level
+//		Debug.Log("Monster collision.");
+		if(other.gameObject.layer.Equals(layerToCollideWith)){
+			myrigidbody.velocity = new Vector2 (myrigidbody.velocity.x * -1, myrigidbody.velocity.y * -1);
+		}
+
+	}
+}
