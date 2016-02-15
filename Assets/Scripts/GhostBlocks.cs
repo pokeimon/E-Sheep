beb802813dc007a035dce5e705c6fff14b0d5c01
Megan Kim <megankim314@gmail.com>
2016-02-15 14:50:50 -0800

    Created ghost block script
    Still work in progress...

---
+++ Assets/Scripts/GhostBlocks.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostBlocks : MonoBehaviour {
+
+	public float fadeTime = 3.0f;
+	public float respawnTime = 4.0f;
+	private bool onBlock = false;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag == "Player") {
+			fadeOut ();	
+		}
+	}
+
+	IEnumerator fadeOut () {
+		yield return new WaitForSeconds (fadeTime);
+		this.tag = "None";
+	}
+
+}
