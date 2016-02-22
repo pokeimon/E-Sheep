a3bdba26e81e234ace40a790c7a4bd14126fecb8
Imon Daneshmand <imondaneshmand@gmail.com>
2016-02-22 15:53:40 -0800

    Key Script
    -Kind of works
    -key gets picked up and shows ghostblock
    -Door/warping does not work

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Key : MonoBehaviour {
+
+	public GameObject PlatForms;
+	public GameObject door;
+	//public Renderer rend;
+
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<SpriteRenderer> ().enabled = true;
+		//rend.enabled = true;
+		door.GetComponent<Door> ().enabled = false;
+		//Sets the Platforms as innactive
+		PlatForms.SetActive(false);
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player") {
+			this.GetComponent<SpriteRenderer> ().enabled = false;
+			//rend.enabled = false;
+			door.GetComponent<Door> ().enabled = true;
+			PlatForms.SetActive(true);
+		}
+	}
+}
