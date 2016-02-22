109a743dd1c60ad3616a1adba2a78b271a15675d
Jorge Manzano <petu32@gmail.com>
2016-02-22 08:04:40 -0800

    animation to skeleton, bone prefab
    rough animation to skeleton , made bone prefab started on script

---
+++ Assets/Scripts/TestScripts/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoneController : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (speed, GetComponent<Rigidbody2D>().velocity.y);
+	}
+	void OnTriggerEnter2D(Collider2D other){
+		Destroy (gameObject);
+	}
+}
