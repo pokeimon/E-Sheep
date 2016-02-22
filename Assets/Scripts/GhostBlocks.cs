8039797d60560cdf792b21919fee10235cc04d46
Megan Kim <megankim314@gmail.com>
2016-02-22 11:44:05 -0800

    Ghost blocks work now

--- Assets/Scripts/GhostBlocks.cs
+++ Assets/Scripts/GhostBlocks.cs
@@ -4,19 +4,21 @@ using System.Collections;
 public class GhostBlocks : MonoBehaviour {
 
 	public Renderer rend;
+	public Collider2D coll;
 	
-	void Start () {  
+	void Start () {
 		rend = GetComponent<Renderer> ();
 		StartCoroutine("HideUnhide");
 	}
-
 
 	IEnumerator HideUnhide () {
 		while (true) {
 			yield return (new WaitForSeconds(2));
 			rend.enabled = true;
+			coll.enabled = true;
 			yield return (new WaitForSeconds(2));
 			rend.enabled = false;
+			coll.enabled = false;
 		}
 	}
 }
