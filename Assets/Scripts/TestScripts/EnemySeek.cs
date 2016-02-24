8d6a5a4a53619fb0b9fac9b1b581b2fbd2a11a8d
gerar_000 <gerard.aguilar.156@my.csun.edu>
2016-02-23 18:11:40 -0800

    Got enemy seek working for flying candy
    Created the range collider prefab for modularity (just drag it into a
    monster object and it should track the player if it enters its range.)

---
+++ Assets/Scripts/TestScripts/EnemySeek.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySeek : MonoBehaviour {
+
+	public Transform enemyBody;
+	void Awake(){
+		enemyBody = gameObject.transform.parent;
+//		targetTransform = GameObject.Find ("Player").GetComponent<Transform> ();
+	}
+
+	void OnTriggerStay2D(Collider2D other){
+		if (other.CompareTag ("Player")) {
+//			Debug.Log ("Within Range.");
+			enemyBody.transform.position = Vector3.MoveTowards(enemyBody.transform.position, other.transform.position, Time.deltaTime*2f);
+		}
+	}
+}
