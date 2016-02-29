5f533875317a21ae13eb2abaf3fe8ee547da91dd
Imon Daneshmand <imondaneshmand@gmail.com>
2016-02-29 14:30:31 -0800

    Working on Saves

--- Assets/Scripts/PlayerSave.cs
+++ Assets/Scripts/PlayerSave.cs
@@ -3,25 +3,71 @@ using System.Collections;
 
 public class PlayerSave : MonoBehaviour {
 
+	//"Name"+saveNum
+	//"Level"+saveNum
+	//"Score"+saveNum
 
-	// Use this for initialization
-	void Start () {
-		if (PlayerPrefs.GetInt ("Level") == null) {
-			PlayerPrefs.SetInt ("Level", 3);
-		}
-		if (PlayerPrefs.GetInt ("Score") == null) {
-			PlayerPrefs.SetInt ("Score", 0);
-		}
-		if (PlayerPrefs.GetInt ("HighScore") == null) {
-			PlayerPrefs.SetInt ("HighScore", 0);
-		}
+	//"PersonalBestCL1"+saveNum
+	//"PersonalBestCL2"+saveNum
+	//"PersonalBestHL1"+saveNum
+
+	//"HighScoreCL1"+rank
+	//"HighScoreCL2"+rank
+	//"HighScoreHL1"+rank
+
+	//"HighscoreNameCL1"+rank
+	//"HighscoreNameCL2"+rank
+	//"HighscoreNameHL1"+rank
+
+	//Level 0 = CL1
+	//Level 1 = CL
+	//Level 2 = HL1
+
+	public void NewSave(int saveNum, string name){
+		PlayerPrefs.SetString ("Name"+saveNum, name);
+		PlayerPrefs.SetInt ("Level"+saveNum, 0);
+		PlayerPrefs.SetInt ("Score"+saveNum, 0);
+		PlayerPrefs.SetInt ("PersonalBestCL1" + saveNum, 0);
+		PlayerPrefs.SetInt ("PersonalBestCL2" + saveNum, 0);
+		PlayerPrefs.SetInt ("PersonalBestHL1" + saveNum, 0);
+	}
+		
+	public void EraseSave(int saveNum){
+		PlayerPrefs.SetString ("Name", null);
+		PlayerPrefs.SetInt ("Level" + saveNum, null);
+		PlayerPrefs.SetInt ("Score" + saveNum, null);
+		PlayerPrefs.SetInt ("PersonalBestCL1" + saveNum, null);
+		PlayerPrefs.SetInt ("PersonalBestCL2" + saveNum, null);
+		PlayerPrefs.SetInt ("PersonalBestHL1" + saveNum, null);
 	}
 
-	void Update () {
-		//HighScore updating
-		if (PlayerPrefs.GetInt ("Score") > PlayerPrefs.GetInt("HighScore")) {
-			PlayerPrefs.SetInt ("HighScore", PlayerPrefs.GetInt ("Score"));
+	public void EndLevel (int level) {
+		int playerNum = PlayerPrefs.GetInt ("SelectedPlayer");
+		string playerPref = ChooseLevel ("PersonalBest", level);
+		int score = PlayerPrefs.GetInt ("Score");
+		if (score > PlayerPrefs.GetInt (playerPref)) {
+			PlayerPrefs.SetInt (playerPref, score);
 		}
+		//Check highscore stuff here too 
+	}
+
+	private string ChooseLevel (string saveNum, int level){
+		string sLevel;
+		switch (level) {
+		case 0:
+			sLevel = "CL1";
+			break;
+		case 1:
+			sLevel = "CL2";
+			break;
+		case 2:
+			sLevel = "HL1";
+			break;
+		default:
+			break;
+		}
+
+		return sLevel;
 	}
 
 
