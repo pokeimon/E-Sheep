a57b9dc588f3e45401e8523b55c1d7508c809ff4
Imon Daneshmand <imondaneshmand@gmail.com>
2016-03-02 12:02:40 -0800

    Player Save
    fixed bugs

--- Assets/Scripts/PlayerSave.cs
+++ Assets/Scripts/PlayerSave.cs
@@ -34,11 +34,11 @@ public class PlayerSave : MonoBehaviour {
 		
 	public void EraseSave(int saveNum){
 		PlayerPrefs.SetString ("Name", null);
-		PlayerPrefs.SetInt ("Level" + saveNum, null);
-		PlayerPrefs.SetInt ("Score" + saveNum, null);
-		PlayerPrefs.SetInt ("PersonalBestCL1" + saveNum, null);
-		PlayerPrefs.SetInt ("PersonalBestCL2" + saveNum, null);
-		PlayerPrefs.SetInt ("PersonalBestHL1" + saveNum, null);
+		PlayerPrefs.SetInt ("Level" + saveNum, 0);
+		PlayerPrefs.SetInt ("Score" + saveNum, 0);
+		PlayerPrefs.SetInt ("PersonalBestCL1" + saveNum, 0);
+		PlayerPrefs.SetInt ("PersonalBestCL2" + saveNum, 0);
+		PlayerPrefs.SetInt ("PersonalBestHL1" + saveNum, 0);
 	}
 
 	public void UpdateScore (int level) {
@@ -61,13 +61,31 @@ public class PlayerSave : MonoBehaviour {
 				if (i != 2) {
 					int num = i + 1;
 					PlayerPrefs.SetInt (highScore + num, PlayerPrefs.GetInt (highScore + i));
-					PlayerPrefs.SetString (highScore + "Name" + num, PlayerPrefs.GetInt (highScore + "Name" + i));
+					PlayerPrefs.SetString (highScore + "Name" + num, PlayerPrefs.GetString (highScore + "Name" + i));
 				}
 				PlayerPrefs.SetInt (highScore + i, score);
 				PlayerPrefs.SetString (highScore + "Name" + i, name);
 			} else
 				break;
 		}
+	}
+
+	public int[,] ReturnScores (){
+		int[,] scores = new int[3, 4];
+		string temp;
+		for (int i = 0; i < 4; i++) {
+			for (int rank = 0; rank < 3; rank++){
+				if (rank != 3) {
+					temp = ChooseLevel ("HighScore", i);
+					scores [rank, i] = PlayerPrefs.GetInt (temp + rank); 
+				} 
+				else {
+					string personalBest = ChooseLevel ("PersonalBest", i) + PlayerPrefs.GetInt("SelectedPlayer");	
+					scores [rank, i] = PlayerPrefs.GetInt (personalBest);
+				}
+			}
+		}
+		return scores;
 	}
 
 	private string ChooseLevel (string sPref, int level){
@@ -83,10 +101,10 @@ public class PlayerSave : MonoBehaviour {
 			sLevel = "HL1";
 			break;
 		default:
+			sLevel = null;
 			break;
 		}
 		return (sPref + sLevel);
 	}
-
-
+		
 }
