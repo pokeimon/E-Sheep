934c23c69abcd30ff2f40ec5ef1c5893458d8582
Ivan <ivan5247@gmail.com>
2016-03-08 16:36:42 -0800

    Fixes
    Fixed null pointer exceptions a better way
    Added 3 rounds for waves
    
    Need:
    a timer to ensure spawning works smoothly.

--- Assets/BossControllerTesting.cs
+++ Assets/BossControllerTesting.cs
@@ -28,22 +28,24 @@ public class BossControllerTesting : MonoBehaviour {
 
 	int wave;
 
+	//Testing purposes (Mein kampf)
+	int e1Health;
+	int e2Health;
+	int e3Health;
 
+	bool e1Active; 
+	bool e2Active;
+	bool e3Active;
 
 	void Start(){
-
-		//Temp solution to null pointer exceptions 
-		actualSummon1 = (GameObject)Instantiate (someEnemy, summonArea.position, summonArea.rotation);
-		actualSummon1.SetActive (false);
-
-		actualSummon2 = (GameObject)Instantiate (someEnemy, summonArea.position, summonArea.rotation);
-		actualSummon2.SetActive (false);
-
-		actualSummon3 = (GameObject)Instantiate (someEnemy, summonArea.position, summonArea.rotation);
-		actualSummon3.SetActive (false);
-
-		bossSummon = (GameObject)Instantiate (bossTongue, summonBoss.position, summonBoss.rotation);
-		bossSummon.SetActive (false);
+		//arbitrary Val
+		e1Health = -3;
+		e2Health = -3;
+		e3Health = -3;
+		//check if enemy is active
+		e1Active = false;
+		e2Active = false;
+		e3Active = false;
 
 		//what enemy to spawn
 		wave = 0;
@@ -58,58 +60,84 @@ public class BossControllerTesting : MonoBehaviour {
 
 	void Update(){
 
-		
-		if (actualSummon1.GetComponent<Health> ().currentHP == 0) {
+		//Debug.Log ("current wave: " + wave);
+		if (e1Active) {
+			e1Health = actualSummon1.GetComponent<Health> ().currentHP;
+		} else if (e2Active) {
+			e2Health = actualSummon2.GetComponent<Health> ().currentHP;
+		} else if (e3Active) {
+			e3Health = actualSummon3.GetComponent<Health> ().currentHP;
+		}
+
+		//if (actualSummon1.GetComponent<Health> ().currentHP == 0) {
+		if(e1Health == 0){
+			//Debug.Log ("e1 is dead " + wave);
 			wave++;
 			theBox.enabled = true;
 		}
-		else if (actualSummon2.GetComponent<Health> ().currentHP == 0) {
+		//else if (actualSummon2.GetComponent<Health> ().currentHP == 0) {
+		else if(e2Health == 0){
+			//Debug.Log ("e2 is dead " + wave);
 			wave++;
 			theBox.enabled = true;
 		}
-		else if (actualSummon3.GetComponent<Health> ().currentHP == 0) {
+		//else if (actualSummon3.GetComponent<Health> ().currentHP == 0) {
+		else if(e3Health == 0){
+			Debug.Log ("e3 is dead " + wave);
 			wave++;
 			theBox.enabled = true;
 		}
 
 	}
-
-
-
+		
 	void FixedUpdate(){
-
 		if (playerEnter) {
+			Debug.Log ("Wave: " + wave);
 			BossEnemies (wave); 
 		}
-
-			
 	}
+		
 
 	void BossEnemies(int n){
-		if (n == 1) {
-			actualSummon1.SetActive (true);
+
+		if (n == 1 || n == 4 || n == 7) {
+			actualSummon1 = (GameObject)Instantiate (someEnemy, summonArea.position, summonArea.rotation);
+			//---------------
+			e1Health = actualSummon1.GetComponent<Health> ().currentHP;
+			e1Active = true;
+			//---------------
 			actualSummon1.GetComponent<Mallow> ().target = playerPosition;
 			actualSummon1.GetComponent<SpriteRenderer> ().color = Color.green;
 			playerEnter = false;
 		}		
-		else if (n == 2) {
-			actualSummon2.SetActive (true);
+		else if (n == 2 || n == 5 || n == 8) {
+			actualSummon2 = (GameObject)Instantiate (someEnemy, summonArea.position, summonArea.rotation);
+			//---------------
+			e2Health = actualSummon2.GetComponent<Health> ().currentHP;
+			e2Active = true;
+			//---------------
 			actualSummon2.GetComponent<Mallow> ().target = playerPosition;
 			actualSummon2.GetComponent<SpriteRenderer> ().color = Color.magenta;
 			playerEnter = false;
 		}	
-		else if (n == 3) {
-			actualSummon3.SetActive (true);
+		else if (n == 3 || n == 6 || n == 9) {
+			actualSummon3 = (GameObject)Instantiate (someEnemy, summonArea.position, summonArea.rotation);
+			//---------------
+			e3Health = actualSummon3.GetComponent<Health> ().currentHP;
+			e3Active = true;
+			//---------------
 			actualSummon3.GetComponent<Mallow> ().target = playerPosition;
 			actualSummon3.GetComponent<SpriteRenderer> ().color = Color.red;
 			playerEnter = false;
 		}	
+		/*
 		else if (n == 4) {
 			
 			bossSummon.SetActive (true);
 			bossSummon.transform.position = summonBoss.position;
 			playerEnter = false;
 		}
+		*/
 
 	}
 
@@ -125,30 +153,53 @@ public class BossControllerTesting : MonoBehaviour {
 			playerPosition = other.transform;
 
 			playerEnter = true;
-			wave++;//wave == 1
+			wave += 1;//wave == 1
 
 			theBox.enabled = false;
 		}
-		else if(actualSummon1.GetComponent<Health> ().currentHP == 0 && other.name == "Player" ){
-			actualSummon1.GetComponent<Health> ().currentHP = -1;
+		//else if(actualSummon1.GetComponent<Health> ().currentHP == 0 && other.name == "Player" ){
+		else if(e1Health == 0 && other.name == "Player"){
+			//actualSummon1.GetComponent<Health> ().currentHP = -1;
+			e1Health = -1;
+			e1Active = false;
+
 			theBox.enabled = false;
-			wave = 2;
+
+			playerEnter = true;
+
+
+
+		}
+		//else if(actualSummon2.GetComponent<Health> ().currentHP == 0 && other.name == "Player" ){
+		else if(e2Health == 0 && other.name == "Player"){
+			//actualSummon2.GetComponent<Health> ().currentHP = -1;
+			e2Health = -1;
+			e2Active = false;
+
+			theBox.enabled = false;
+		
 			playerEnter = true;
 
 		}
-		else if(actualSummon2.GetComponent<Health> ().currentHP == 0 && other.name == "Player" ){
-			actualSummon2.GetComponent<Health> ().currentHP = -1;
+		//else if(actualSummon3.GetComponent<Health> ().currentHP == 0 && other.name == "Player" ){
+		else if(e3Health == 0 && other.name == "Player"){
+			//actualSummon3.GetComponent<Health> ().currentHP = -1;
+
+			e3Health = -1;
+			e3Active = false;
+
 			theBox.enabled = false;
-			wave = 3;
+
 			playerEnter = true;
 
-		}
-		else if(actualSummon3.GetComponent<Health> ().currentHP == 0 && other.name == "Player" ){
-			actualSummon3.GetComponent<Health> ().currentHP = -1;
-			theBox.enabled = false;
-			wave = 4;
-			playerEnter = true;
+			//temp to stop wave counter
+			if(wave >= 10){
+				playerEnter = false;
+			}
+
 		}
 
 	}
 }
+
+
