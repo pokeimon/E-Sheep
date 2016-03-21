2def3bf56ec03ac86122732d730afc84ca7be571
Ivan <ivan5247@gmail.com>
2016-03-20 22:06:26 -0700

    Boss Head
    Sample Boss head for Candy Land II
    
    Testing using Steven's rotation script

---
+++ Assets/Boss_Rotation_Testing.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss_Rotation_Testing : MonoBehaviour {
+
+	public Transform player;
+	public Transform bossHead;
+	public Collider2D c;
+	public float headAngle;
+
+
+	// Use this for initialization
+	void Start () {
+		c = gameObject.GetComponent<Collider2D>();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		headAngle = 57.296f * Mathf.Atan2((player.position.y - bossHead.position.y), (player.position.x - bossHead.position.x));
+
+	}
+
+	void OnTriggerStay2D(Collider2D col){
+		if(col.gameObject.tag.Equals ("Player")){
+			bossHead.eulerAngles = new Vector3 (0, 0, headAngle + 145);
+		}
+	}
+}
+
