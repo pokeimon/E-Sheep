2ad5f6f05025fc57cd78a55bc396dddd9f2f91d7
celularbeing <ivan5247@gmail.com>
2016-03-24 23:03:15 -0700

    Script Clean up
    added comments, cleaned up the boss animation

--- Assets/Boss_Rotation_Testing.cs
+++ Assets/Boss_Rotation_Testing.cs
@@ -3,33 +3,37 @@ using System.Collections;
 
 public class Boss_Rotation_Testing : MonoBehaviour {
 
+	//Objects in Scene
 	public Transform player;
-	public Transform bossHead;
-	public float headAngle;
 	public GameObject tongueRange;
 	public GameObject theTongue;
 
+	public float headAngle;
+	public Transform bossHead;
 
 	private Animator animator;
-
-
 	// Use this for initialization
 	void Start () {
+		bossHead.eulerAngles = new Vector3 (0,0,0);
 		animator = GetComponent<Animator> ();
 	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
-		//headAngle = 57.296f * Mathf.Atan2((player.position.y - bossHead.position.y), (player.position.x - bossHead.position.x));
-		headAngle = tongueRange.GetComponent<RangeAngle>().shotAngle;
-		bossHead.eulerAngles = new Vector3 (0,0, headAngle + 145);
-
-		if (theTongue.GetComponent<FrogTongue> ().autostart) {
-			animator.SetBool("bossFireReady", true);
-		} else {
-			animator.SetBool("bossFireReady", false);
+		//This is to check if the player is within the circle collider
+		//Using this method to clean up the way the boss head interaction looks in game 
+		if (tongueRange.GetComponent<RangeAngle> ().fire) {
+			headAngle = tongueRange.GetComponent<RangeAngle> ().shotAngle;
+			bossHead.eulerAngles = new Vector3 (0, 0, headAngle + 180);
+			if (theTongue.GetComponent<FrogTongue> ().autostart) {
+				animator.SetBool ("bossFireReady", true);
+			} else {
+				animator.SetBool ("bossFireReady", false);
+			}
+		} else {//outside circle collider, boss head doesn't move, mouth is closed
+			animator.SetBool ("bossFireReady", false);
+			bossHead.eulerAngles = new Vector3 (0,0,0);
 		}
-
 	}
 
 }
