a4f6161427cf8507d7384ec6677246cecb63d976
Megan Kim <megankim314@gmail.com>
2016-03-29 11:08:09 -0700

    Still working on character selection scene

--- Assets/Scripts/CharSelect.cs
+++ Assets/Scripts/CharSelect.cs
@@ -4,40 +4,37 @@ using UnityEngine.UI;
 
 public class CharSelect : PlayerSave {
 	//private Component saveScript = GameObject.Find("GM").GetComponent<PlayerSave>();
-	public Button NG0;
-	public Button NG1;
-	public Button NG2;
-	public Button save;
-	public Button delete;
+	public Button newGame0;
+	public Button newGame1;
+	public Button newGame2;
+	//public Button save;
+	//public Button delete;
 	public Button enter;
-	public Canvas EnterCharName;
-	public Canvas Char0;
-	public Canvas Char1;
-	public Canvas Char2;
+	public Canvas enterCharName;
+	//public Canvas char0;
+	//public Canvas char1;
+	//public Canvas char2;
 	public InputField name;
 
 
 	// Use this for initialization
 	void Start () {
-		NG0 = NG0.GetComponent<Button> ();
-		NG1 = NG1.GetComponent<Button> ();
-		NG2 = NG2.GetComponent<Button> ();
-		save = save.GetComponent<Button> ();
-		delete = delete.GetComponent<Button> ();
+		newGame0 = newGame0.GetComponent<Button> ();
+		newGame1 = newGame1.GetComponent<Button> ();
+		newGame2 = newGame2.GetComponent<Button> ();
+		//save = save.GetComponent<Button> ();
+		//delete = delete.GetComponent<Button> ();
 		enter = enter.GetComponent<Button> ();
-		Char0 = Char0.GetComponent<Canvas> ();
-		Char1 = Char1.GetComponent<Canvas> ();
-		Char2 = Char2.GetComponent<Canvas> ();
-		EnterCharName = EnterCharName.GetComponent<Canvas> ();
+
+		//char0 = char0.GetComponent<Canvas> ();
+		//char1 = char1.GetComponent<Canvas> ();
+		//char2 = char2.GetComponent<Canvas> ();
+		enterCharName = enterCharName.GetComponent<Canvas> ();
 		name = name.GetComponent<InputField> ();
 
-		EnterCharName.enabled = false;
-		save.enabled = false;
-		delete.enabled = false;
-		enter.enabled = false;
-		name.enabled = false;
+		enterCharName.enabled = false;
 
-
+		/*
 		//Char 0
 		if (IsNewGame (0)) {
 			Char0.enabled = false;
@@ -55,11 +52,8 @@ public class CharSelect : PlayerSave {
 			Char2.enabled = false;
 		} else
 			NG2.enabled = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+		*/
 	}
 
 	private bool IsNewGame (int select) {
@@ -69,17 +63,16 @@ public class CharSelect : PlayerSave {
 			return false;
 	}
 
-	private void NewGame (int select) {
-		EnterCharName.enabled = true; //turns on character name canvas
-//		NewSave (select, name);
-		enter.enabled = true;
+	public void PressNewGame (int select) {
+		enterCharName.enabled = true; //turns on character name canvas
+
+		//enter.enabled = true;
 		PlayerPrefs.SetInt ("SelectedPlayer", select);
 	}
 
-	void PressEnter () {
+	public void PressEnter () {
 		Application.LoadLevel (1);
 	}
-
 
 
 
