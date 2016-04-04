fa155a0330f47418a360bf264402519231b287bc
Steven Blachowiak <Stblack004@gmail.com>
2016-04-04 12:30:47 -0700

    Some work on limiting the angle of the Tongue
    - Ivan we need to talk.

--- Assets/Scripts/CandyLandBoss/RangeAngle.cs
+++ Assets/Scripts/CandyLandBoss/RangeAngle.cs
@@ -8,7 +8,8 @@ public class RangeAngle : MonoBehaviour {
 	public Transform playerOnEnter;
 	public Transform tongue;
 	public Collider2D c;
-	public float shotAngle;
+	public float shotAngle = 0;
+	public float tempAngle;
 	public bool fire = false;
 
 	private FrogTongue frogTongue;
@@ -24,14 +25,18 @@ public class RangeAngle : MonoBehaviour {
 		if (fire == false) {
 			c.enabled = true;
 		} else {
-			shotAngle = 57.296f * Mathf.Atan2((player.position.y - tongue.position.y), (player.position.x - tongue.position.x));
+			//tempAngle = 90 + 57.296f * Mathf.Atan2 ((player.position.y - tongue.position.y), (player.position.x - tongue.position.x));
+			//if ((tempAngle > -79 && tempAngle < 100) || (tempAngle > 250 && tempAngle < 280)) {
+			//	shotAngle = tempAngle;
+			//}
+			shotAngle = 90 + 57.296f * Mathf.Atan2 ((player.position.y - tongue.position.y), (player.position.x - tongue.position.x));
 		}
 	}
 
 	void OnTriggerStay2D(Collider2D col){
 		if(col.gameObject.tag.Equals ("Player")){
 			fire = true;
-			tongue.eulerAngles = new Vector3 (0, 0, shotAngle+90);
+			tongue.eulerAngles = new Vector3 (0, 0, shotAngle);
 			playerOnEnter.position = new Vector2 (player.position.x, player.position.y);
 			if (frogTongue.autostart == true) {
 				c.enabled = false;
