0364c7b20d824110ca1e049c369bb43c1b569df3
Aaron Scott <AaronPScott@gmail.com>
2016-04-25 00:05:44 -0700

    Added Enemies and Coins to Candy Land
    - Added a lot more enemies to CandyLand1
    - Added coins to CandyLand1
    - Added coins to HorrorLand in previous push
    - Added falling Candy Corns

--- Assets/Scripts/TestScripts/BranchBreak.cs
+++ Assets/Scripts/TestScripts/BranchBreak.cs
@@ -9,7 +9,6 @@ public class BranchBreak : MonoBehaviour
 {
     private Rigidbody2D breakrigidbody2d;
 
-
     //When the player enters the trigger collider, drop this object's rigidbody
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -19,19 +18,9 @@ public class BranchBreak : MonoBehaviour
             breakrigidbody2d = GetComponentInChildren<Rigidbody2D>();
             //Debug.Log(breakrigidbody2d.name);
             breakrigidbody2d.isKinematic = false;
+			gameObject.SetActive(false);
 
         }
-    }
-    
-    //When the object's rigidbody exits the trigger collider, disable it (otherwise it'll get in the player's way)
-    void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.name == "Breakable Branch")
-        {
-            other.gameObject.SetActive(false);
-            other.GetComponentInParent<BoxCollider2D>().enabled = false;
-        }
-
     }
 }
 
