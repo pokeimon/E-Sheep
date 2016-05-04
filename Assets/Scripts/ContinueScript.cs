f142feeec10e40d38de01dc9577e8c09cf0953ee
gerar_000 <gerard.aguilar.156@my.csun.edu>
2016-05-04 04:01:57 -0700

    ContinueScripts that are attached to the Camera

---
+++ Assets/Scripts/ContinueScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContinueScript : MonoBehaviour {
+
+	void Awake(){
+		//check PlayerPrefs
+		if (Application.loadedLevelName.Equals ("Horror Land")) {
+//			Debug.Log ("I have awoken!");
+			if (PlayerPrefs.GetInt ("ContinueFlag") == 1) {
+//				Debug.Log ("Here I am!");
+				Vector3 newRespawn = new Vector3 (
+					                     PlayerPrefs.GetFloat ("RespawnX"),
+					                     PlayerPrefs.GetFloat ("RespawnY"),
+					                     PlayerPrefs.GetFloat ("RespawnZ")
+				                     );
+
+				GameObject.Find ("Player").transform.position = newRespawn;
+			}
+		} else {
+			PlayerPrefs.SetFloat ("RespawnX", 0f);
+			PlayerPrefs.SetFloat ("RespawnY", 0f);
+			PlayerPrefs.SetFloat ("RespawnZ", 0f);
+			PlayerPrefs.SetInt ("ContinueFlag", 0);
+		}
+	}
+
+
+}
